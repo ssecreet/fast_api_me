ce0d36bfdde6fc6870ba312ddaa8c1e8e3092577
ssecreet <120476413+ssecreet@users.noreply.github.com>
2023-03-18 18:04:56 +0300

    Update test_get_employee_by_id_wrong_fields.py

--- tests/employees/test_get_employee_by_id_wrong_fields.py
+++ tests/employees/test_get_employee_by_id_wrong_fields.py
@@ -8,4 +8,4 @@ import pytest
 def test_negative(employee_fixture, employee_id, exp_code):
     response = employee_fixture.api_client.employee.get_employee_by_id(employee_id)
     assert response.status_code == exp_code, "Статус код не соответствует ожидаемому"
-    assert employee_fixture.checkers.validate_json(response.json(), "schemas/employee.json")
+#     assert employee_fixture.checkers.validate_json(response.json(), "schemas/employee.json")
